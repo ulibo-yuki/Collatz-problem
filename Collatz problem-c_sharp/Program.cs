619a7dddc0d07eceb2cef27482dc491c69a86abd
ulibo <doublet-entrees.0x@icloud.com>
2023-10-14 15:30:25 +0900

    first commit

---
+++ Collatz problem-c_sharp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Program{
+  class Program{
+    static void Main(string[] args){
+      Console.WriteLine("値を入力してください。");
+      int n = int.Parse(Console.ReadLine());
+      if (n == null || n == 1){
+        Console.WriteLine("正しい値を入力してください。");
+        n = int.Parse(Console.ReadLine());
+      }
+
+      while (n != 1){
+        int k = Even_Odd(n);
+        if (k == 0){
+          n /= 2;
+        }if (k == 1){
+          n *= 3;
+          n += 2;
+        }
+        Console.WriteLine(n);
+        
+      }
+      Console.WriteLine("end.");
+      
+    }
+
+    private static int Even_Odd (int i){
+      i %= 2;
+      if (i == 0){
+        return 0;
+      }else{
+        return 1;
+      }
+    }
+  }
+}
